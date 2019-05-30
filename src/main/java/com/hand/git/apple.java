4c7436420ee9e36d87e5059c7a8025e682e59d25
王睿 <@LAPTOP-MTFG5U49>
2019-05-30 09:06:13 +0800

    edit word

--- src/main/java/com/hand/git/apple.java
+++ src/main/java/com/hand/git/apple.java
@@ -3,6 +3,6 @@ package com.hand.git;
 public class apple {
 	public static void main(String[] args) {
 		System.out.println("edit");
-		System.out.println("http");
+		System.out.println("word");
 	}
 }
