abcdbda37895b42a46dca3297ba627f5593d3305
王睿 <@LAPTOP-MTFG5U49>
2019-05-30 10:27:09 +0800

    edit edit

--- src/main/java/com/hand/git/apple.java
+++ src/main/java/com/hand/git/apple.java
@@ -3,9 +3,5 @@ package com.hand.git;
 public class apple {
 	public static void main(String[] args) {
 		System.out.println("edit");
-		System.out.println("good");
-		System.out.println("crollter");
-		System.out.println("word");
-		System.out.println("good");
 	}
 }
